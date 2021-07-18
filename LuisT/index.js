917967daafb3932a657bb625031893b3e67a03d9
Luis Tapia <luis.antonio.tapia80@gmail.com>
2021-07-17 22:48:40 -0500

    Fixed delete route path to work with query parameters

--- LuisT/index.js
+++ LuisT/index.js
@@ -7,59 +7,58 @@ app.use(express.json())
 
 // Creates a POST route with a path of /addPerson
 // Calls the URL: http://java-sample-api-2020.herokuapp.com/addPerson
-app.post('/addPerson', (postmanRequest, postmanResponse) => {
-    //console.log(postmanRequest.body);
+app.post('/addPerson', (req, res) => {
+    //console.log(req.body);
 
-    const body = postmanRequest.body;
+    const body = req.body;
 
     axios.post('http://java-sample-api-2020.herokuapp.com/addPerson', body)
     .then( apiResponse => {
         //console.log(apiResponse.data);
-        postmanResponse.status(200).json({'message':'person added'});       // Confirm success
+        res.status(200).json({'message':'person added'});       // Confirm success
     })
     .catch(function(err) {
-        postmanResponse.status(200).json({'message':'there was an error'}); // Otherwise, notify of error.
+        res.status(200).json({'message':'there was an error'}); // Otherwise, notify of error.
     })
 })
 
 // Creates a GET route with a path of /getAllPeople
 // Calls the URL: http://java-sample-api-2020.herokuapp.com/getAllPeople
-app.get('/getAllPeople', (postmanRequest, postmanResponse) => {
-    //console.log(postmanRequest.body);
+app.get('/getAllPeople', (req, res) => {
+    //console.log(req.body);
 
-    const body = postmanRequest.body;
+    const body = req.body;
 
     axios.get('http://java-sample-api-2020.herokuapp.com/getAllPeople', body)
     .then( apiResponse => {
         //console.log(apiResponse.data);
-        postmanResponse.status(200).json(apiResponse.data);
+        res.status(200).json(apiResponse.data);
     })
     .catch(function(err) {
-        postmanResponse.status(200).json({'message':'there was an error'});
+        res.status(200).json({'message':'there was an error'});
     })
 })
 
 // Creates a DELETE route with a path of /deletePerson/:UserId
 // Calls the URL: http://java-sample-api-2020.herokuapp.com/deletePerson/:UserId
-app.delete('/deletePerson/:UserId', (postmanRequest, postmanResponse) => {
-    //console.log(postmanRequest.body);
+app.delete('/deletePerson', (req, res) => {
+    // console.log(req.body);
+    // console.log(req.params);
+    // console.log(req.query);
+    
+    const idNum = req.query['id'];    
+    const body = req.body;
 
-    const body = postmanRequest.body;
-
-    axios.delete('http://java-sample-api-2020.herokuapp.com' + postmanRequest.path, body)
+    axios.delete('http://java-sample-api-2020.herokuapp.com/deletePerson?id=' + idNum, body)
     .then( apiResponse => {
         //console.log(apiResponse.data);
-        postmanResponse.status(200).json({'message':'person deleted'});
+        res.status(200).json({'message':'person deleted'});
     })
     .catch(function(err) {
-        postmanResponse.status(200).json({'message':'there was an error'});
+        res.status(200).json({'message':'there was an error'});
     })
 })
 
-/* 
-    Listen for incoming requests.
-    The first argument to app.listen is the port that we want the API to listen on.
-    In this case we use port 8080. The second argument is a function that will
-    display a message to the terminal saying that our API is now listening
-*/
+// Listen for incoming requests.
+// Test route paths.
 app.listen(8080, () => console.log(`Example app listening at http://localhost:8080`))
