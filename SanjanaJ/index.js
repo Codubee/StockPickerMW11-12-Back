23ba1a0825c69ad8b65b0668fbcdb8fdf50f84d6
sanjanajadhavv <sanjanaua2013@gmail.com>
2021-07-20 20:04:23 -0500

    arrow functions

--- SanjanaJ/index.js
+++ SanjanaJ/index.js
@@ -5,59 +5,61 @@ app.use(express.json())
 
 // creates a POST route to add people
 // http://java-sample-api-2020.herokuapp.com/addPerson
-app.post('/addPerson', function (postmanRequest, postmanResponse) {
-    console.log(postmanRequest.body)
-
+app.post('/addPerson', (postmanRequest, postmanResponse) => 
+{
     const body = postmanRequest.body
 
     // add person and display message
     axios.post('http://java-sample-api-2020.herokuapp.com/addPerson', body)
-    .then(function(apiResponse) {
+    .then((apiResponse) =>
+    {
         console.log(apiResponse.data)
         postmanResponse.status(200).json({'message': 'person added'})
     })
 
     // catch error and display error message
-    .catch(function(err) {
+    .catch((err)=>
+    {
         postmanResponse.status(500).json({'message': 'there was an error'})
     })
 })
 
 // creates a GET route to get all people
 // http://java-sample-api-2020.herokuapp.com/getAllPeople
-app.get('/getAllPeople', function (postmanRequest, postmanResponse) {
-
-    console.log(postmanRequest.body)
-
+app.get('/getAllPeople', (postmanRequest, postmanResponse) =>
+{
     // get all people and display message
     axios.get('http://java-sample-api-2020.herokuapp.com/getAllPeople')
-    .then(function(apiResponse) {
+    .then((apiResponse) =>
+    {
         console.log(apiResponse.data)
         postmanResponse.status(200).json({'message': 'all people displayed'})
     })
 
     // catch error and display error message
-    .catch(function(err) {
+    .catch((err) =>
+    {
         postmanResponse.status(500).json({'message': 'there was an error'})
     })
 })
 
 // creates a DELETE route to delete people
 // http://java-sample-api-2020.herokuapp.com/deletePerson
-app.delete('/deletePerson', function (postmanRequest, postmanResponse) {
-    console.log(postmanRequest.body)
-
+app.delete('/deletePerson', (postmanRequest, postmanResponse) =>
+{
     const idNum = postmanRequest.query['id']
 
     // delete person and display message
     axios.delete('http://java-sample-api-2020.herokuapp.com/deletePerson?id=' + idNum)
-    .then(function(apiResponse) {
+    .then((apiResponse) =>
+    {
         console.log(apiResponse.data)
         postmanResponse.status(200).json({'message': 'person deleted'})
     })
 
     // catch error and display error message
-    .catch(function(err) {
+    .catch((err) =>
+    {
         postmanResponse.status(500).json({'message': 'there was an error'})
     })
 })
