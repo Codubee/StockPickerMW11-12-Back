cea311df0de4c4bcaf26af9ce014343852520950
Luis Tapia <luis.antonio.tapia80@gmail.com>
2021-07-28 14:43:59 -0500

    Route to get stock from db

--- index.js
+++ index.js
@@ -3,5 +3,18 @@ const app = express()
 const axios = require('axios')
 app.use(express.json())
 
+// Route to get a stock from the database of stocks.
+// Use GET route with a path of /
+// Calls the URL: https://codubee-projects-api.herokuapp.com/stocks/getStockData
+app.get('/getStockData', (req, res) => {
+    axios.get('https://codubee-projects-api.herokuapp.com/stocks/getStockData')
+    .then( apiResponse => {
+        res.status(200).json(apiResponse.data);
+    })
+    .catch( function(err) {
+        res.status(200).json({'message':'there was an error getting stock from db'});
+    })
+})
+
 
 app.listen(8080, () => console.log('Example app listening at http://localhost:8080'))
