d5c96c20ac58d84391f14dd8db9a86d671551771
sanjanajadhavv <sanjanaua2013@gmail.com>
2021-08-01 22:28:08 -0500

    route for weather

--- index.js
+++ index.js
@@ -3,5 +3,23 @@ const app = express()
 const axios = require('axios')
 app.use(express.json())
 
+// creates a GET route to get all people
+// http://java-sample-api-2020.herokuapp.com/getAllPeople
+app.get('/getWeather', (postmanRequest, postmanResponse) =>
+{
+    // get the weather
+    axios.get('https://codubee-projects-api.herokuapp.com')
+    .then((apiResponse) =>
+    {
+        console.log(apiResponse.data)
+        postmanResponse.status(200).json(apiResponse.data)
+    })
+
+    // catch error and display error message
+    .catch((err) =>
+    {
+        postmanResponse.status(500).json({'message': 'there was an error'})
+    })
+})
 
 app.listen(8080, () => console.log('Example app listening at http://localhost:8080'))
