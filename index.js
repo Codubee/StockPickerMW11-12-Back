8102f32b91bba5eeceec47dfd8162284aca0bbe1
codubee <info@codubee.com>
2021-08-04 11:52:17 -0500

    Added evn port to index.js

--- index.js
+++ index.js
@@ -33,4 +33,4 @@ app.post('/addStock', (req, res) =>
             res.status(500).json({ 'message': 'There was an error' })
         })
 })
-app.listen(8080, () => console.log('Example app listening at http://localhost:8080'))
+app.listen( process.env.PORT || 8080, () => console.log('Example app listening at http://localhost:8080'))
