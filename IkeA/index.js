2afa40b5464ff2c3a92eab9263a1a75bd4a49cb1
Ikechukwu Akujobi <iykejobi@gmail.com>
2021-08-11 17:07:59 -0400

    added dotenv library

--- IkeA/index.js
+++ IkeA/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const axios = require('axios')
 app.use(express.json())
+require('dotenv').config();
 
 // Create a POST route with a path of /addPerson
 app.post('/addPerson', (req, res) =>
